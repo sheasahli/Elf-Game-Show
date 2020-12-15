3d5cdbd1484fa1dc4085fb91013f149df81152c5
sheasahli <shea.sahli@gmail.com>
2020-12-15 13:38:50 -0800

    phrases array + start button event listener

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
 const qwerty = document.querySelector('#qwerty');
 const phrase = document.querySelector('#phrase');
 const startButton = document.querySelector('btn__reset');
-
+const phrases = ["singing loud for all to hear", "christmas spirit",
+                "talking but louder", "smiling", "the Nice list"];
 var missed = 0;
+
+
+// listen for the start button to be pressed
+startButton.addEventListener('click', => {
+  overlay.style.display = 'none';
+});
