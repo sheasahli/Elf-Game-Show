a7dfbaf2c1f1a8fd82d3fd0a440921d6f73ece5c
sheasahli <shea.sahli@gmail.com>
2020-12-16 14:04:34 -0800

    questions + randomIndex

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const phrase = document.querySelector('#phrase');
 const startButton = document.querySelector('.btn__reset');
 const phrases = ["singing loud for all to hear", "christmas spirit",
                 "talking but louder", "smiling", "the Nice list"];
+const questions = ['What is the best way to spread Christmas Cheer?',
+                  'What makes reindeer fly?', 'Buddy thinks singing is like?',
+                  'What does Buddy like to do?', 'Where is there always room for more?'];
 var missed = 0;
+var randomIndex = Math.floor(Math.random() * questions.length)
 
 // listen for the start button to be pressed
 startButton.addEventListener('click', () => {
