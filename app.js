804612fd4a7db76c3be0a288eeca688793dd237c
sheasahli <shea.sahli@gmail.com>
2020-12-16 15:04:35 -0800

    adjust to let and add randomQuestion function

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const phrases = ["singing loud for all to hear", "christmas spirit",
 const questions = ['What is the best way to spread Christmas Cheer?',
                   'What makes reindeer fly?', 'Buddy thinks singing is like?',
                   'What does Buddy like to do?', 'Where is there always room for more?'];
-var missed = 0;
-var randomIndex = Math.floor(Math.random() * questions.length);
+let missed = 0;
+let randomIndex = Math.floor(Math.random() * questions.length);
 
 // listen for the start button to be pressed
 startButton.addEventListener('click', () => {
@@ -19,4 +19,11 @@ function getRandomPhraseAsArray (arrP) {
       let randomPhrase = arrP[randomIndex];
       let phrasesParse = Object.values(randomPhrase);
       return phraseParse;
+}
+
+// function to return corresponding question
+
+function getRandomQuestions (arrQ) {
+        let randomQueston = arrQ[randomIndex];
+        return randomQuestion;
 }
